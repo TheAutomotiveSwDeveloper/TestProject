bf214e4322dee23685916f481aa0628001ff9278
TheAutomotiveSwDeveloper <nilesh.p.garud@gmail.com>
2019-06-15 18:52:25 +0530

    Added sum function in hello work code

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+int SumFunction(int a, int b);
 
 int main(void)
 {
+	int Addition;
 	printf("Hellow World\n");
 	printf("Added changes\n");
-	
+	Addition = SumFunction(10,10);
+	printf("Sum=%d",Addition);
 	return 0;
-	
+}
+
+int SumFunction(int a, int b)
+{
+	int Result;
+	Result = a + b;
+	return Result;
 }
